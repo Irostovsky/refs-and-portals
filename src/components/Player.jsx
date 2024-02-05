cb2fa262558cea44cc353d3b17ae4d015434370c
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2024-02-05 11:05:39 +0200

    Add Player and TimeChallenge components.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 export default function Player() {
+  const inputPlayerName = useRef();
+  const [currentPlayerName, setCurrentPlayerName] = useState("");
+
+  const handleClick = () => {
+    setCurrentPlayerName(inputPlayerName.current.value);
+    inputPlayerName.current.value = "";
+  };
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {currentPlayerName || "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputPlayerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
